7616b7cfb83f89442c8bd28a4d73332385c3c58f
Emmanuel <emmanuel@abstrak.agency>
2024-09-21 22:05:36 +0200

    Save

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://api-masvingo.privateutilityzim.com/api',
+    apiUrl: 'https://api-masvingo.abstrak.agency/api',
     tokenStorageKey: "msvg_prd_token",
     userStorageKey: "msvg_prd_user",
 };
